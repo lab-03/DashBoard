12e5f435d96cb8058f32da831fa959ecde61a3ec
IIZODIACII <amr_x14@outlook.com>
2020-08-02 20:01:22 +0200

    Modify QrCode page's ui

--- src/components/QrCode/QrCode.js
+++ src/components/QrCode/QrCode.js
@@ -15,7 +15,7 @@ const QrCode = props => {
 
   useEffect(() => {
     let { listening, socket } = state;
-    console.log(listening);
+    console.log({ listening });
     if (listening === false) {
       console.log(hash);
       socket.connect();
@@ -131,7 +131,7 @@ const QrCode = props => {
 
   const endQrCode = hash => {
     fetch("http://localhost:8888/api/qrcodes/end", {
-      method: "post",
+      method: "put",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         hash
@@ -145,38 +145,39 @@ const QrCode = props => {
   };
   let { hide, attendees } = state;
   return (
-    <div>
-      {!hide ? (
-        <Fragment>
-          <div className="center ma">
+    <Fragment>
+      <div className="mt5 flex justify-around">
+        <div className="w-100">
+          <DashBoard
+            attendees={attendees}
+            onAttendeeAdd={onAttendeeAdd}
+            onAttendeeUpdate={onAttendeeUpdate}
+            onAttendeeDelete={onAttendeeDelete}
+          />
+        </div>
+        {!hide ? (
+          <Fragment>
             <img
+              className="w-40 mr5"
               id="inputimage"
               alt="QrCode"
               src={imageUrl}
-              width="500px"
-              height="auto"
             />
-          </div>
-          <div className="center ma3">
-            <Button
-              variant="contained"
-              className="grow shadow"
-              color="primary"
-              onClick={() => endQrCode(hash)}
-            >
-              End Session
-            </Button>
-          </div>
-        </Fragment>
-      ) : null}
-      <div>
-        <DashBoard
-          attendees={attendees}
-          onAttendeeAdd={onAttendeeAdd}
-          onAttendeeUpdate={onAttendeeUpdate}
-          onAttendeeDelete={onAttendeeDelete}
-        />
+          </Fragment>
+        ) : null}
       </div>
+      {!hide ? (
+        <div className="center ma2 flex flex-row-reverse">
+          <Button
+            variant="contained"
+            className="grow shadow"
+            color="primary"
+            onClick={() => endQrCode(hash)}
+          >
+            End Session
+          </Button>
+        </div>
+      ) : null}
       {hide ? (
         <div className="center ma3 flex justify-around">
           <div>
@@ -207,7 +208,7 @@ const QrCode = props => {
           </div>
         </div>
       ) : null}
-    </div>
+    </Fragment>
   );
 };
 
