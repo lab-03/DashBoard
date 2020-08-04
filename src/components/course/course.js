5539bc55fd7fdca2511ceb07e6100d03b41902e6
IIZODIACII <amr_x14@outlook.com>
2020-08-04 21:33:04 +0200

    Remove wrong import

--- src/components/course/course.js
+++ src/components/course/course.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import crypto from "crypto";
-import "./course.css";
 import {
   ListItem,
   ListItemSecondaryAction,
