478c80e477692d5e0fccc131f38f6c4d835da8ba
Ahmad Shaaban <ahmedshaaban.fci@gmail.com>
2020-08-04 20:17:31 +0200

    Change ui of sign in page

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,45 +1,116 @@
 import React, { useState } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import RaisedButton from "material-ui/RaisedButton";
-import TextField from "material-ui/TextField";
-import background from "../../background.jpg";
-import Title from "../TitleComponent/TitleComponent";
+import background from "../../4.jpg";
+import shapee from "../../shape1.png";
 import { withStyles } from "@material-ui/core/styles";
-import Logos from "../logos/logos";
-import SignUp from "../signup/signup";
 import { toast } from "react-toastify";
-// import { PlacesAirportShuttle } from "material-ui/svg-icons";
+
 let backStyle = {
   backgroundImage: `url(${background})`,
-  height: "1340px",
+  height: "750px",
+
   backgroundSize: "cover",
   backgroundPosition: "center",
-  backgroundAttachment: "fixed"
+  backgroundAttachment: "fixed",
 };
-let stylee = {
-  marginLeft: 793
+let shapeStyle = {
+  position: "fixed",
+  top: 50,
+  left: 800,
+  width: "45%",
 };
+
 let layer = {
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  backgroundColor: "rgba(0, 0, 0, 0)",
   position: "absolute",
   top: 0,
   left: 0,
   width: "100%",
-  height: "1340px",
-  backgroundAttachment: "fixed"
+  height: "750px",
+  backgroundAttachment: "fixed",
 };
 
 const styles = {
   input: {
-    color: "white"
-  }
+    color: "white",
+  },
 };
-const Signin = props => {
+let EmailStyle = {
+  position: "fixed",
+  left: 900,
+  top: 150,
+  width: "32.5%",
+  height: "10%",
+
+  textAlign: "center",
+  fontSize: "20px",
+  borderRadius: "10px",
+  borderWidth: "0.1px",
+  borderColor: "#c2c2c2",
+  backgroundColor: "white",
+
+  border: "2px solid #E8E8E8",
+  fontFamily: "Arial",
+};
+let PasswordStyle = {
+  width: "32.5%",
+  position: "fixed",
+  left: 900,
+  top: 250,
+
+  height: "10%",
+
+  textAlign: "center",
+  fontSize: "20px",
+  borderRadius: "10px",
+  borderWidth: "0.1px",
+  borderColor: "#c2c2c2",
+  backgroundColor: "white",
+
+  border: "2px solid #E8E8E8",
+  fontFamily: "Arial",
+};
+let ButtonStyle = {
+  position: "fixed",
+  left: 900,
+  top: 350,
+  height: "10%",
+  width: "32.5%",
+
+  textAlign: "center",
+  fontSize: "25px",
+  borderRadius: "10px",
+  borderWidth: "0.1px",
+  borderColor: "#c2c2c2",
+  color: "white",
+  backgroundColor: "#7f7aea",
+
+  border: "0px solid #E8E8E8",
+  fontFamily: "Arial",
+};
+let SignUpStyle = {
+  position: "fixed",
+  left: 900,
+  top: 450,
+  height: "10%",
+  width: "32.5%",
+
+  textAlign: "center",
+  fontSize: "25px",
+  borderRadius: "10px",
+  borderWidth: "0.1px",
+  borderColor: "#c2c2c2",
+  color: "white",
+  backgroundColor: "#faa551",
+
+  border: "0px solid #E8E8E8",
+  fontFamily: "Arial",
+};
+const Signin = (props) => {
   const [info, setInfo] = useState({
     Email: "",
-    Password: ""
+    Password: "",
   });
-  const InfoHandler = e => {
+  const InfoHandler = (e) => {
     const updatedInfo = { ...info, [e.target.name]: e.target.value };
     setInfo(updatedInfo);
   };
@@ -50,54 +121,40 @@ const Signin = props => {
   return (
     <div style={backStyle}>
       <div style={layer}>
-        <MuiThemeProvider>
-          <div style={stylee}>
-            <TextField
-              hintText="Enter your Email"
-              color="primary"
-              hintStyle={{ color: "white" }}
-              name="Email"
-              floatingLabelText="Email"
-              floatingLabelStyle={{ color: "white" }}
-              value={info.Email}
-              onChange={InfoHandler}
-            />
-            <TextField
-              type="password"
-              name="Password"
-              ink-color="white"
-              hintText="Enter your Password"
-              hintStyle={{ color: "white" }}
-              floatingLabelText="Password"
-              floatingLabelStyle={{ color: "white" }}
-              value={info.Password}
-              onChange={InfoHandler}
-            />
-            <RaisedButton
-              label="Login"
-              primary={true}
-              style={style}
-              onClick={ButtonHandler}
-            />
-          </div>
-        </MuiThemeProvider>
-        <Title />
-        <Logos />
         <div
           style={{
             backgroundColor: "rgb(255,255,255,0.8)",
             backgroundSize: "auto",
-            borderRadius: "130px"
+            //borderRadius: "130px",
           }}
-        >
-          <SignUp />
-        </div>
+        ></div>
+        <img src={shapee} alt="box" style={shapeStyle} />
+
+        <form>
+          <input
+            type="text"
+            name="Email"
+            style={EmailStyle}
+            placeholder="Email Address"
+            value={info.Email}
+            onChange={InfoHandler}
+          ></input>
+          <input
+            type="password"
+            name="Password"
+            style={PasswordStyle}
+            placeholder="Password"
+            value={info.Password}
+            onChange={InfoHandler}
+          ></input>
+          <button style={ButtonStyle} onClick={ButtonHandler}>
+            Log In
+          </button>
+          <button style={SignUpStyle}>Sign Up</button>
+        </form>
       </div>
     </div>
   );
 };
 
-const style = {
-  margin: 15
-};
 export default withStyles(styles)(Signin);
